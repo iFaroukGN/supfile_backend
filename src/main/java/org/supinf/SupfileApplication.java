d712b82ab7a1f5ec8adac882a294f133dae4c4e2
EdemAMEGBETO <38034025+EdemAMEGBETO@users.noreply.github.com>
2018-04-07 04:30:14 +0200

    Initialisation projet spring boot
    ajout des dépendances starter-web,  starter-data-jpa

---
+++ src/main/java/org/supinf/SupfileApplication.java
@@ -0,0 +1,12 @@
+package org.supinf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupfileApplication.class, args);
+	}
+}
---
+++ src/test/java/org/supinf/SupfileApplicationTests.java
@@ -0,0 +1,16 @@
+package org.supinf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupfileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
