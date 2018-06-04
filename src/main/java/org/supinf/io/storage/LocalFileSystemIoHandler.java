bfc2b176d35a306a6a137603931b4191eff66c34
CodewithBlue <279529@supinfo.com>
2018-06-04 10:37:46 +0200

    io.storage package main classes

---
+++ src/main/java/org/supinf/io/storage/LocalFileSystemIoHandler.java
@@ -0,0 +1,43 @@
+package org.supinf.io.storage;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Classe pour interagir avec le système de fichiers local
+ *
+ * @author BLU Kwensy Eli
+ */
+@Component
+public class LocalFileSystemIoHandler implements StorageAccessProvider {
+
+    @Override
+    public void createResource(Resource resource) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createFile(FileResource fileResource) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createFolder(FolderResource folderResource) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void moveResource(Resource source, FolderResource destination) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteResource(Resource resource) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void renameResource(Resource resource, String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
